619c44adea20e192495eb349c8a6ef7621022eec
Alexander Shvets <neochief@shvetsgroup.com>
2017-03-12 18:09:31 +0200

    Fixed problem with outdated content cache.

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,7 @@
 Route::group(['middleware' => ['static-cache']], function () {
     Route::get('robots.txt', 'RobotsTxtController@robots');
     Route::get('sitemap.xml', 'SiteMapController@sitemap');
-    Route::get('ajax/jetpages/timestamp.json', 'PageController@getContentTimestamp');
+    Route::get('ajax/jetpages/timestamp', 'PageController@getContentTimestamp');
 
     // Specific override for a front page to overcome default laravel's route in app/Http/routes.php
     Route::get('/', 'PageController@show');
