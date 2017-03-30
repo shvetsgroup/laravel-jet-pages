f2deb76685296f568af05583632fef82e195d984
Alexander Shvets <neochief@shvetsgroup.com>
2017-03-31 02:14:52 +0300

    Timestamp.

--- src/routes.php
+++ src/routes.php
@@ -3,9 +3,10 @@
 Route::group(['middleware' => ['static-cache']], function () {
     Route::get('robots.txt', 'RobotsTxtController@robots');
     Route::get('sitemap.xml', 'SiteMapController@sitemap');
-    Route::get('ajax/jetpages/timestamp', 'PageController@getContentTimestamp');
 
     // Specific override for a front page to overcome default laravel's route in app/Http/routes.php
     Route::get('/', 'PageController@show');
     Route::get('{all}', 'PageController@show')->where(['all' => '.*']);
 });
+
+Route::get('ajax/jetpages/timestamp', 'PageController@getContentTimestamp');
